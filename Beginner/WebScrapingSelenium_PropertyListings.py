68f1398a0bca0a053f9b4fb56ebd24f90473165f
Hakkun <hammad.ak96@gmail.com>
2025-02-18 16:56:21 +0800

    multiple webpages scraping

--- Beginner/WebScrapingSelenium_PropertyListings.py
+++ Beginner/WebScrapingSelenium_PropertyListings.py
@@ -1,59 +1,81 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import undetected_chromedriver as uc
 import json
 import pandas as pd
 import time
 
 #Set up the Selenium WebDriver
-driver = webdriver.Chrome()
+driver = uc.Chrome()
 #Open URL and send GET
-url = "https://www.propertyguru.com.my/bm/rumah-banglo-dijual"
-driver.get(url)
-#adding delay for page to load
-time.sleep(5)
-#Locate the <script> tag with the JSON Data
-script_tag = driver.find_element(By.XPATH, '//script[@id="__NEXT_DATA__"]')
-#Extract the JSON Content
-json_data = script_tag.get_attribute("innerHTML")
-#Parse the JSON Data
-data = json.loads(json_data)
-#Close the browser
-driver.quit()
-
-listings = data["props"]["pageProps"]["pageData"]["data"]["listingsData"]
-
+base_url = "https://www.propertyguru.com.my/bm/rumah-banglo-dijual"
 property_data = []
 
-for listing in listings:
-    listing_data = listing["listingData"]
+for page in range(1,101): #take first 100 pages data
+    #condition for url fitting
+    if page == 1:
+        url = base_url
+    else:
+        url = f"{base_url}/{page}"
+    driver.get(url)
+    #adding delay for page to load
+    try:
+        WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, '//script[@id="__NEXT_DATA__"]')))
+    except Exception as e:
+        print(f"Error loading page {page}: {e}")
+        continue
+    #Locate the <script> tag with the JSON Data
+    script_tag = driver.find_element(By.XPATH, '//script[@id="__NEXT_DATA__"]')
+    #Extract the JSON Content
+    json_data = script_tag.get_attribute("innerHTML")
+    #Parse the JSON Data
+    data = json.loads(json_data)
 
-    #Extract relevant fields
-    name = listing_data.get("localizedTitle", "N/A")
-    price = listing_data["price"].get("pretty","N/A")
-    price_perArea = listing_data.get("pricePerArea", {}).get("localeStringValue", "N/A")
-    size_floor = listing_data["listingFeatures"][1][0].get("text", "N/A") 
-    size_land = listing_data["area"].get("localeStringValue", "N/A")
-    location = listing_data.get("fullAddress", "N/A")
-    bedrooms = listing_data["listingFeatures"][0][0].get("text", "N/A")
-    bathrooms = listing_data["listingFeatures"][0][1].get("text", "N/A")
-    property_type = listing_data["property"].get("typeText", "N/A")
-    url = listing_data.get("url","N/A")
+    listings = data["props"]["pageProps"]["pageData"]["data"]["listingsData"]
 
-    #Append data to the list
-    property_data.append({
-        "Name" : name,
-        "Price" : price,
-        "Price (Per Area)" : price_perArea,
-        "Size (Floor)" : size_floor,
-        "Size (Land)" : size_land,
-        "Location" : location,
-        "Bedrooms" : bedrooms,
-        "Bathrooms" : bathrooms,
-        "Type of Property" : property_type,
-        "URL" : url
-    })
+    for listing in listings:
+        listing_data = listing["listingData"]
 
+        #Extract relevant fields
+        name = listing_data.get("localizedTitle", "N/A")
+        price = listing_data["price"].get("pretty","N/A")
+        price_perArea = listing_data.get("pricePerArea", {}).get("localeStringValue", "N/A")
+        if len(listing_data["listingFeatures"]) > 1:
+            size_floor = listing_data["listingFeatures"][1][0].get("text", "N/A") 
+        else :
+            size_floor = "N/A"
+        size_land = listing_data["area"].get("localeStringValue", "N/A")
+        location = listing_data.get("fullAddress", "N/A")
+        bedrooms = listing_data["listingFeatures"][0][0].get("text", "N/A") if len(listing_data["listingFeatures"][0]) > 0 else "N/A"
+        bathrooms = listing_data["listingFeatures"][0][1].get("text", "N/A") if len(listing_data["listingFeatures"][0]) > 1 else "N/A"
+        property_type = listing_data["property"].get("typeText", "N/A")
+        url = listing_data.get("url","N/A")
+
+        #Append data to the list
+        property_data.append({
+            "Name" : name,
+            "Price" : price,
+            "Price (Per Area)" : price_perArea,
+            "Size (Floor)" : size_floor,
+            "Size (Land)" : size_land,
+            "Location" : location,
+            "Bedrooms" : bedrooms,
+            "Bathrooms" : bathrooms,
+            "Type of Property" : property_type,
+            "URL" : url
+        })
+    #Delay between each pages
+    time.sleep(2)
+
+#Close the browser
+try:
+    driver.quit()
+except OSError:
+    pass
+#Convert to DataFrame
 df = pd.DataFrame(property_data)
 df.to_csv("property_listings.csv", index= False)
 print("Data successfully saved to CSV files!")
